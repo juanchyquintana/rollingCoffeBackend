cceba4904e8a3fc0c626ac69039e63d58591aae2
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-03-04 10:54:30 -0300

    tomamos el puerto de una varibale de entorno, pero en caso contrario usamos el puerto 2504

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-import express from 'express'
-const app = express()
-const port = 2504
+import express from "express";
+const app = express();
 
-app.set('port', port)
-app.listen(app.get('port'), () => {
-    console.log(`El Backend está funcionando en el puerto ${port}`)
-})
+app.set("port", process.env.PORT || 2504);
+app.listen(app.get("port"), () => {
+  console.log(`El Backend está funcionando en el puerto ${app.get("port")}`);
+});
