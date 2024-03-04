52f95435c7993136660e68343c6d7921cf0cde11
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-03-04 11:18:22 -0300

    se agregaron los middlewars cors morgan y dotenv. Se los configuró en el index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import "dotenv/config";
+
 const app = express();
 
 app.set("port", process.env.PORT || 2504);
 app.listen(app.get("port"), () => {
   console.log(`El Backend está funcionando en el puerto ${app.get("port")}`);
 });
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
