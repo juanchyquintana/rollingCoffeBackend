e0e419e96309fe556fe72f95adda36e48c570781
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-03-06 11:34:38 -0300

    se conectó a la base de datos de mongodb y se instaló mongoose

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import "dotenv/config";
 import { fileURLToPath } from "url";
 import path from "path";
 import productosRouter from "./src/routes/productosRoutes.js";
+import './src/database/database.js'
 
 const app = express();
 
