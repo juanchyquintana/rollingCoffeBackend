5eb8ad0d33c4aedcc84c01ca14a1fe389c1bc02e
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-03-06 10:05:42 -0300

    agregamos un middleware para cargar el index como archivo static

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import "dotenv/config";
+import { fileURLToPath } from "url";
+import path from "path";
 
 const app = express();
 
@@ -15,8 +17,12 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/nuevo", (req, res) => {
   console.log("alguien solicito algo");
 
-  res.send('Qué queres pelear?')
+  res.send("Qué queres pelear?");
 });
