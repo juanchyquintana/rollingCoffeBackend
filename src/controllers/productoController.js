54163a859b3e80ee1695fbc22702380cbff64986
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-03-13 11:24:32 -0300

    se creó la funcionalidad del editar producto

--- src/controllers/productoController.js
+++ src/controllers/productoController.js
@@ -24,12 +24,27 @@ const crearProductos = async (req, res) => {
 
 const obtenerProducto = async (req, res) => {
   try {
-    const producto = await Producto.findById(req.params.id)
-    res.status(200).json(producto)
+    const producto = await Producto.findById(req.params.id);
+    res.status(200).json(producto);
   } catch (error) {
-    console.log(error)
-    res.status(404).json({ mensaje: 'No se pudo encontrar el Producto'})
+    console.log(error);
+    res.status(404).json({ mensaje: "No se pudo encontrar el Producto" });
   }
 };
 
-export { listarProductos, crearProductos, obtenerProducto };
+const editarProducto = async (req, res) => {
+  try {
+    const buscarProducto = await Producto.findById(req.params.id);
+    if (!buscarProducto) {
+      return res.status(404).json({ mensaje: "Producto No Encontrado" });
+    }
+
+    await Producto.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json({ mensaje: "Producto Editado Correctamente!" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al Editar el Producto" });
+  }
+};
+
+export { listarProductos, crearProductos, obtenerProducto, editarProducto };
--- src/routes/productosRoutes.js
+++ src/routes/productosRoutes.js
@@ -3,6 +3,7 @@ import {
   listarProductos,
   crearProductos,
   obtenerProducto,
+  editarProducto
 } from "../controllers/productoController.js";
 
 const router = Router();
@@ -11,5 +12,6 @@ router.get("/productos", listarProductos);
 router.post("/productos", crearProductos);
 
 router.get("/producto/:id", obtenerProducto);
+router.put('/producto/:id', editarProducto)
 
 export default router;
