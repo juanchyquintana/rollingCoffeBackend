ff77b1bb5b75defdb5e07e8b34f76ac3f8a2635f
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-03-13 10:05:30 -0300

    agregamos la logica correspondiente para obtener un array de productos

--- src/controllers/productoController.js
+++ src/controllers/productoController.js
@@ -1,21 +1,25 @@
-import Producto from '../database/model/Producto.js'
+import Producto from "../database/model/Producto.js";
 
 const listarProductos = async (req, res) => {
-  console.log("holaaaa");
-
-  res.send("enviar lista de productos..");
+  try {
+    const productos = await Producto.find();
+    res.status(200).json(productos)
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ mensaje: "No se pudieron Listar los Productos" });
+  }
 };
 
 const crearProductos = async (req, res) => {
   try {
-    const productoNuevo = await Producto.create(req.body)
+    const productoNuevo = await Producto.create(req.body);
 
-    await productoNuevo.save()
-    res.status(201).json({ mensaje: "Producto creado correctamente!"})
+    await productoNuevo.save();
+    res.status(201).json({ mensaje: "Producto creado correctamente!" });
   } catch (error) {
-    console.log(error)
-    res.status(400).json({ mensaje: "Error al crear el Producto" })
+    console.log(error);
+    res.status(400).json({ mensaje: "Error al crear el Producto" });
   }
-}
+};
 
 export { listarProductos, crearProductos };
